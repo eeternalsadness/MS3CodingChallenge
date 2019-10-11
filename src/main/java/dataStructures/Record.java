674920cc5e662434a07f76cf87ea9bb00b7218b0
eeternalsadness <45772281+eeternalsadness@users.noreply.github.com>
2019-10-11 06:30:10 -0400

    Change tabs to 4 spaces for better legibility

--- src/main/java/dataStructures/Record.java
+++ src/main/java/dataStructures/Record.java
@@ -9,61 +9,61 @@
 package dataStructures;
 
 public class Record{
-	private final int size;
-	private String[] record;
+    private final int size;
+    private String[] record;
     private Record nextRecord;
-	private boolean isValid;
+    private boolean isValid;
 
-	public Record(){
-	    this.size = 10;
-	    this.record = new String[size];
-		this.nextRecord = null;
-		this.isValid = true;
-	}
+    public Record(){
+        this.size = 10;
+        this.record = new String[size];
+        this.nextRecord = null;
+        this.isValid = true;
+    }
 
-	//get the data from record at a specific index
-	public String getData(int column) throws ArrayIndexOutOfBoundsException{
-		//if the stored data is null, return an empty string
-		if(this.record[column] == null){
-			return "";
-		}
-		return this.record[column];
-	}
+    //get the data from record at a specific index
+    public String getData(int column) throws ArrayIndexOutOfBoundsException{
+        //if the stored data is null, return an empty string
+	if(this.record[column] == null){
+            return "";
+        }
+        return this.record[column];
+    }
 
-	//set the value for the data at a specific index
-	public boolean setData(int column, String data){
-		try{
-		    this.record[column] = data;
+    //set the value for the data at a specific index
+    public boolean setData(int column, String data){
+        try{
+            this.record[column] = data;
             //if the data is empty, the record is invalid
-			//assume no data contains just whitespaces
+            //assume no data contains just whitespaces
             if (data.isEmpty()){
                 this.isValid = false;
             }
 
             return true;
         }
-		catch(ArrayIndexOutOfBoundsException e){
-		    return false;
+        catch(ArrayIndexOutOfBoundsException e){
+            return false;
         }
-	}
+    }
 
-	//get the next record in the table
-	public Record getNext(){
-		return nextRecord;
-	}
+    //get the next record in the table
+    public Record getNext(){
+        return nextRecord;
+    }
 
-	//set the next record in the table
-	public boolean setNext(Record record){
-		this.nextRecord = record;
-		return true;
-	}
+    //set the next record in the table
+    public boolean setNext(Record record){
+        this.nextRecord = record;
+        return true;
+    }
 
-	public int getSize(){
-	    return this.size;
+    public int getSize(){
+        return this.size;
     }
 
     //return true if the record is valid
-	public boolean isValid(){
-		return this.isValid;
-	}
+    public boolean isValid(){
+        return this.isValid;
+    }
 }
